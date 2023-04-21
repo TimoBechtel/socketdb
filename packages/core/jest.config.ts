161c24cc0c6cc8dcbfb092ed648405334d7eedc0
Timo Bechtel <hi@timobechtel.com>
2023-04-21 13:42:16 +0200

    chore: nx migrate
    migrate nx to latest version

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,13 +1,14 @@
 export default {
 	displayName: 'core',
 	preset: '../../jest.preset.js',
-	globals: {
-		'ts-jest': {
-			tsconfig: '<rootDir>/tsconfig.spec.json',
-		},
-	},
+	globals: {},
 	transform: {
-		'^.+\\.[tj]s$': 'ts-jest',
+		'^.+\\.[tj]s$': [
+			'ts-jest',
+			{
+				tsconfig: '<rootDir>/tsconfig.spec.json',
+			},
+		],
 	},
 	moduleFileExtensions: ['ts', 'js', 'html'],
 	coverageDirectory: '../../coverage/packages/core',
