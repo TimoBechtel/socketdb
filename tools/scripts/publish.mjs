778cb438f733634221318b92198cd0fb0e2fe213
Timo Bechtel <hi@timobechtel.com>
2023-06-02 10:49:49 +0200

    build: fix publish script

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -7,10 +7,12 @@
  * You might need to authenticate with NPM before running this script.
  */
 
-import { readCachedProjectGraph } from '@nx/devkit';
+import pkg from '@nx/devkit';
+const { readCachedProjectGraph } = pkg;
+
+import chalk from 'chalk';
 import { execSync } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
-import chalk from 'chalk';
 
 function invariant(condition, message) {
 	if (!condition) {
